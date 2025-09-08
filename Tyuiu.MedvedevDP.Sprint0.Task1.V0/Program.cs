7c35f6f21480444e8fa194ebbb63cfb6703bd230
danii <danii@DANIIL>
2025-09-08 14:41:53 +0500

    Task1

---
+++ Tyuiu.MedvedevDP.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.MedvedevDP.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+
+            string name = "Nya";
+            int age = 22;
+
+            Console.WriteLine("Privet , my name is "+name+" , mne " + age + " let "  );
+
+        }
+    }
+}
