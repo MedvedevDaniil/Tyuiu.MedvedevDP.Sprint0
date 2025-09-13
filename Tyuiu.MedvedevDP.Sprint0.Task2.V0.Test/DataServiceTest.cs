d0e74c5511bf3c1eeb5d838cb313e4998558d31a
danii <danii@DANIIL>
2025-09-13 23:25:54 +0500

    Task2

---
+++ Tyuiu.MedvedevDP.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.MedvedevDP.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.MedvedevDP.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Daniil";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Privet, Daniil", res);
+        }
+    }
+}
